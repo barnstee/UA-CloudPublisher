bc046cee1729ea6e0f28ba0eeb6eb5d7d016c505
Erich Barnstedt <erichb@microsoft.com>
2023-06-04 10:30:53 +0200

    Moved diagnostics logging from log file to OPC UA PubSub messages and send it to the cloud instead. also small cleanup of diagnostics UI labels.

--- Diagnostics.cs
+++ Diagnostics.cs
@@ -102,23 +102,23 @@ namespace Opc.Ua.Cloud.Publisher
                     _hubClient.AddOrUpdateTableEntry("OPC UA monitored items", Info.NumberOfOpcMonitoredItemsMonitored.ToString());
                     _hubClient.AddOrUpdateTableEntry("OPC UA monitored items queue capacity", Settings.Instance.InternalQueueCapacity.ToString());
                     _hubClient.AddOrUpdateTableEntry("OPC UA monitored items queue current items", Info.MonitoredItemsQueueCount.ToString());
-                    _hubClient.AddOrUpdateTableEntry("OPC UA monitored item notifications enqueued", Info.EnqueueCount.ToString());
-                    _hubClient.AddOrUpdateTableEntry("OPC UA monitored item notifications enqueue failure", Info.EnqueueFailureCount.ToString());
+                    _hubClient.AddOrUpdateTableEntry("OPC UA monitored items enqueued", Info.EnqueueCount.ToString());
+                    _hubClient.AddOrUpdateTableEntry("OPC UA monitored items enqueue failure", Info.EnqueueFailureCount.ToString());
                     _hubClient.AddOrUpdateTableEntry("Messages sent to broker", Info.SentMessages.ToString());
                     _hubClient.AddOrUpdateTableEntry("Last successful broker message sent @", Info.SentLastTime.ToString());
                     _hubClient.AddOrUpdateTableEntry("Total bytes sent to broker", Info.SentBytes.ToString());
                     _hubClient.AddOrUpdateTableEntry("Average broker message size (bytes)", (Info.SentBytes / (Info.SentMessages == 0 ? 1 : Info.SentMessages)).ToString());
                     _hubClient.AddOrUpdateTableEntry("Average broker message latency (ms)", Info.AverageMessageLatency.ToString(), true);
                     _hubClient.AddOrUpdateTableEntry("Average broker messages/second sent", messagesPerSecond.ToString(), true);
-                    _hubClient.AddOrUpdateTableEntry("Average number of notifications batched in broker message", Info.AverageNotificationsInBrokerMessage.ToString());
-                    _hubClient.AddOrUpdateTableEntry("Average number of OPC UA notifications/second sent", (messagesPerSecond * Info.AverageNotificationsInBrokerMessage).ToString(), true);
-                    _hubClient.AddOrUpdateTableEntry("broker message send failures", Info.FailedMessages.ToString());
-                    _hubClient.AddOrUpdateTableEntry("broker messages too large to send to broker", Info.TooLargeCount.ToString());
+                    _hubClient.AddOrUpdateTableEntry("Average number of OPC UA monitored items batched in broker message", Info.AverageNotificationsInBrokerMessage.ToString());
+                    _hubClient.AddOrUpdateTableEntry("Average number of OPC UA monitored items/second sent", (messagesPerSecond * Info.AverageNotificationsInBrokerMessage).ToString(), true);
+                    _hubClient.AddOrUpdateTableEntry("Broker message send failures", Info.FailedMessages.ToString());
+                    _hubClient.AddOrUpdateTableEntry("Broker messages too large to send to broker", Info.TooLargeCount.ToString());
                     _hubClient.AddOrUpdateTableEntry("Missed broker message send intervals", Info.MissedSendIntervalCount.ToString());
-                    _hubClient.AddOrUpdateTableEntry("Number of OPC UA notifications encoded", Info.NumberOfEvents.ToString());
+                    _hubClient.AddOrUpdateTableEntry("Number of OPC UA monitored items encoded", Info.NumberOfEvents.ToString());
                     _hubClient.AddOrUpdateTableEntry("Current working set in MB", (Process.GetCurrentProcess().WorkingSet64 / (1024 * 1024)).ToString());
-                    _hubClient.AddOrUpdateTableEntry("broker send interval setting (s)", Settings.Instance.DefaultSendIntervalSeconds.ToString());
-                    _hubClient.AddOrUpdateTableEntry("broker message size setting (bytes)", Settings.Instance.BrokerMessageSize.ToString());
+                    _hubClient.AddOrUpdateTableEntry("Broker send interval setting (s)", Settings.Instance.DefaultSendIntervalSeconds.ToString());
+                    _hubClient.AddOrUpdateTableEntry("Broker message size setting (bytes)", Settings.Instance.BrokerMessageSize.ToString());
 
                     chartValues.Add(Info.AverageMessageLatency.ToString());
                     chartValues.Add(messagesPerSecond.ToString());
@@ -128,30 +128,20 @@ namespace Opc.Ua.Cloud.Publisher
                     // write to the log at 10x slower than the UI diagnostics
                     if (ticks % 10 == 0)
                     {
-                        _logger.LogInformation($"UACloudPublisher started @ {Info.PublisherStartTime}");
-                        _logger.LogInformation($"Connected to broker: {Info.ConnectedToBroker}");
-                        _logger.LogInformation($"OPC UA sessions: {Info.NumberOfOpcSessionsConnected}");
-                        _logger.LogInformation($"OPC UA subscriptions: {Info.NumberOfOpcSubscriptionsConnected}");
-                        _logger.LogInformation($"OPC UA monitored items: {Info.NumberOfOpcMonitoredItemsMonitored}");
-                        _logger.LogInformation($"OPC UA monitored items queue capacity: {Settings.Instance.InternalQueueCapacity}");
-                        _logger.LogInformation($"OPC UA monitored items queue current items: {Info.MonitoredItemsQueueCount}");
-                        _logger.LogInformation($"OPC UA monitored item notifications enqueued: {Info.EnqueueCount}");
-                        _logger.LogInformation($"OPC UA monitored item notifications enqueue failure: {Info.EnqueueFailureCount}");
-                        _logger.LogInformation($"Messages sent to broker: {Info.SentMessages}");
-                        _logger.LogInformation($"Last successful broker message sent @: {Info.SentLastTime}");
-                        _logger.LogInformation($"Total bytes sent to broker: {Info.SentBytes}");
-                        _logger.LogInformation($"Average broker message size (bytes): {Info.SentBytes / (Info.SentMessages == 0 ? 1 : Info.SentMessages)}");
-                        _logger.LogInformation($"Average broker message latency (ms): {Info.AverageMessageLatency}");
-                        _logger.LogInformation($"Average broker messages/second sent: {messagesPerSecond}");
-                        _logger.LogInformation($"Average number of notifications batched in broker message: {Info.AverageNotificationsInBrokerMessage}");
-                        _logger.LogInformation($"Average number of OPC UA notifications/second sent: {messagesPerSecond * Info.AverageNotificationsInBrokerMessage}");
-                        _logger.LogInformation($"broker message send failures: {Info.FailedMessages}");
-                        _logger.LogInformation($"broker messages too large to send to broker: {Info.TooLargeCount}");
-                        _logger.LogInformation($"Missed broker message send intervals: {Info.MissedSendIntervalCount}");
-                        _logger.LogInformation($"Number of OPC UA notifications encoded: {Info.NumberOfEvents}");
-                        _logger.LogInformation($"Current working set in MB: {Process.GetCurrentProcess().WorkingSet64 / (1024 * 1024)}");
-                        _logger.LogInformation($"broker send interval setting (s): {Settings.Instance.DefaultSendIntervalSeconds}");
-                        _logger.LogInformation($"broker message size setting (bytes): {Settings.Instance.BrokerMessageSize}");
+                        DiagnosticsSend("ConnectedToBroker", new DataValue(Info.ConnectedToBroker));
+                        DiagnosticsSend("NumOpcSessions", new DataValue(Info.NumberOfOpcSessionsConnected));
+                        DiagnosticsSend("NumOpcSubscriptions", new DataValue(Info.NumberOfOpcSubscriptionsConnected));
+                        DiagnosticsSend("NumOpcMonitoredItems", new DataValue(Info.NumberOfOpcMonitoredItemsMonitored));
+                        DiagnosticsSend("QueueCapacity", new DataValue((int)Settings.Instance.InternalQueueCapacity));
+                        DiagnosticsSend("QueueCount", new DataValue(Info.MonitoredItemsQueueCount));
+                        DiagnosticsSend("EnqueueFailures", new DataValue(Info.EnqueueFailureCount));
+                        DiagnosticsSend("SentMessages", new DataValue(Info.SentMessages));
+                        DiagnosticsSend("BrokerMessageSize", new DataValue(Info.SentBytes / (Info.SentMessages == 0 ? 1 : Info.SentMessages)));
+                        DiagnosticsSend("BrokerMessageLatency", new DataValue(Info.AverageMessageLatency));
+                        DiagnosticsSend("BrokerMessagesSecond", new DataValue(messagesPerSecond));
+                        DiagnosticsSend("NumOpcMonitoredItemsSecond", new DataValue(messagesPerSecond * Info.AverageNotificationsInBrokerMessage));
+                        DiagnosticsSend("BrokerMessageSendFailures", new DataValue(Info.FailedMessages));
+                        DiagnosticsSend("CurrentWorkingSetMBs", new DataValue(Process.GetCurrentProcess().WorkingSet64 / (1024 * 1024)));
                     }
 
                     _lastNumMessagesSent = Info.SentMessages;
@@ -160,6 +150,30 @@ namespace Opc.Ua.Cloud.Publisher
                 {
                     _logger.LogError(ex, "writing diagnostics output causing error");
                 }
+            }
+        }
+
+        private void DiagnosticsSend(string displayName, DataValue value)
+        {
+            value.ServerTimestamp = DateTime.UtcNow;
+
+            try
+            {
+                MessageProcessorModel messageData = new MessageProcessorModel
+                {
+                    ExpandedNodeId = "nsu=http://opcfoundation.org/UA/CloudPublisher/;s=" + displayName,
+                    ApplicationUri = "urn:localhost:UACloudPublisher",
+                    MessageContext = ServiceMessageContext.GlobalContext,
+                    Name = displayName,
+                    Value = value
+                };
+
+                // enqueue the message
+                MessageProcessor.Enqueue(messageData);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Message for diagnostics failed with {ex.Message}'.");
             }
         }
     }
