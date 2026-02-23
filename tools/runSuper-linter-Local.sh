eceb4ca7cdf85f74fcbd61b171ecaf113535fc1e
Goetz Goerisch <g.goerisch@vdw.de>
2026-02-23 14:43:35 +0100

    feat: update linter settings
    Signed-off-by: Goetz Goerisch <g.goerisch@vdw.de>

--- tools/runSuper-linter-Local.sh
+++ tools/runSuper-linter-Local.sh
@@ -4,12 +4,22 @@
 REPO_DIR=$(pwd)
 echo "$REPO_DIR"
 
+docker pull ghcr.io/super-linter/super-linter:v8.5.0
+
 # Run the Docker container with the specified environment variables and volume mount
 docker run \
-	-e FILTER_REGEX_EXCLUDE='.*wwwroot/lib/.*' \
-	-e IGNORE_GITIGNORED_FILES=true \
+	-e FILTER_REGEX_EXCLUDE='.*wwwroot/.*' \
 	-e LOG_LEVEL=INFO \
 	-e DEFAULT_BRANCH=origin/develop \
 	-e RUN_LOCAL=true \
 	-e VALIDATE_ALL_CODEBASE=true \
-	-v "$REPO_DIR:/tmp/lint" -it --rm ghcr.io/super-linter/super-linter:v7.1.0
+	-e VALIDATE_BIOME_LINT=false \
+	-e VALIDATE_BIOME_FORMAT=false \
+	-e VALIDATE_TRIVY=false \
+	-e VALIDATE_CHECKOV=false \
+	-e VALIDATE_GIT_COMMITLINT=false \
+	-e VALIDATE_GITHUB_ACTIONS_ZIZMOR=false \
+	-e ENABLE_GITHUB_PULL_REQUEST_SUMMARY_COMMENT=false \
+	-e VALIDATE_TRIVY=false \
+	-e VALIDATE_PYTHON_BLACK=false \
+	-v "$REPO_DIR:/tmp/lint" -it --rm ghcr.io/super-linter/super-linter:v8.5.0
